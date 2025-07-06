bf715f05e5276eb8cdc7b858b02c596876cb2039
Prashant Timilsina <157892231+PrashantTimilsina@users.noreply.github.com>
2025-07-06 20:44:51 +0545

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import BlogCartWrapper from "@/components/BlogData";
 import Hero from "@/components/Hero";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <Hero />
